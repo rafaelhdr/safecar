79c095e13d15bb0c224ac754f94564abc22b1cda
Rafael <rafael@hurpia.com.br>
2014-08-25 11:47:12 -0300

    Simple page for status and map

--- safecar/urls.py
+++ safecar/urls.py
@@ -5,6 +5,8 @@ admin.autodiscover()
 
 urlpatterns = patterns('',
     url(r'^$', 'web.views.home', name='home'),
+    url(r'^dashboard$', 'web.views.dashboard', name='dashboard'),
+    url(r'^map$', 'web.views.map', name='map'),
 
     url(r'^admin/', include(admin.site.urls)),
 )
--- web/views.py
+++ web/views.py
@@ -6,3 +6,11 @@ from django.template import RequestContext
 def home(request):
 	context = {}
 	return render_to_response('web/home.html', context, context_instance=RequestContext(request))
+
+def dashboard(request):
+	context = {}
+	return render_to_response('web/dashboard.html', context, context_instance=RequestContext(request))
+
+def map(request):
+	context = {}
+	return render_to_response('web/map.html', context, context_instance=RequestContext(request))
